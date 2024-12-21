176dbd9cffad08b9bb2a4d3446c24de5e57951c9
Viddesh1 <viddeshkagithub@gmail.com>
2024-12-21 16:40:06 +0530

    This code sucks!
    The recursion of the function is not good enough to understand
    that if the root is smaller than child then swap.
    
    This way of the code is not enough and needs to be changed.
    
    Unncessary call to buildheap() function is not needed.
    Nor the mapheapify call to function every time.

--- 8.cpp
+++ 8.cpp
@@ -1,6 +1,6 @@
 // ------------------------------------------------------------------------------------------------------------------------------------
 // Write a program to insert the element in the maximum heap.
-// Max Heap visualization: https://www.cs.usfca.edu/~galles/visualization/Heap.html
+// Min Heap visualization: https://www.cs.usfca.edu/~galles/visualization/Heap.html
 // Note:- This program does not work has it should be.
 //  For array[3, 6, 10, 12, 15] output should be -> a[15, 12, 6, 3, 10]
 // ------------------------------------------------------------------------------------------------------------------------------------
@@ -113,21 +113,21 @@ int length(int a[])
     return i;
 }
 
-// output
+// output :- 
+// (Not checking if the 15 > 3 and then swap and then again 15 > 10 swap)
 /*
 $ ./8
+$ ./8
 Enter the number of elements: 5
-Enter a value: 2
-
 Enter a value: 3
-2
-Enter a value: 4
-3 2
-Enter a value: 1
-4 2 3
-Enter a value: 5
-4 2 3 1
-Enter a value: 2
-4 5 3 1 2
-4 5 3 1 2
+
+Enter a value: 6
+3
+Enter a value: 10
+6 3
+Enter a value: 12
+10 3 6
+Enter a value: 15
+10 12 6 3
+10 12 6 3 15
 */
